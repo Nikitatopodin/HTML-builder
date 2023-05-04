7faec927f7b627a6dcc36efb398039d598f4283a
Nikitatopodin <Nikitatop1@gmail.com>
2023-05-04 13:14:57 +0300

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin: input, stdout: output } = require('process');
+const readline = require('node:readline');
+
+const filePath = path.join(__dirname, 'text.txt');
+const rl = readline.createInterface({ input, output });
+
+fs.createWriteStream(filePath);
+
+output.write('Введите текст, который хотите добавить, или exit, если хотите закончить\n');
+
+rl.on('line', (input) => {
+  if (input.trim() === 'exit') {
+    console.log('Всего Вам доброго!');
+    process.exit();
+  }
+  fs.appendFile(
+    filePath,
+    input + '\n',
+    err => {
+      if (err) throw err;
+    }
+  );
+});
+rl.on('SIGINT', () => {
+  console.log('Всего Вам доброго!');
+  process.exit();
+});
