afce00442db1a3097a9931f2ad04fca8a5af3750
Nikitatopodin <Nikitatop1@gmail.com>
2023-05-04 18:06:14 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const { mkdir, readdir, copyFile } = require('node:fs/promises');
+const { stat } = require('node:fs');
+const { join } = require('node:path');
 
+
+
+async function copyFiles() {
+  try {
+    const newDirPath = join(__dirname, 'files-copy');
+    const dirPath = join(__dirname, 'files');
+    await mkdir(newDirPath, { recursive: true });
+    const files = await readdir(dirPath);
+
+    for (const file of files)
+      stat(
+        join(dirPath, file),
+        async (err, stats) => {
+          if (err) throw err;
+          if (stats.isFile()) {
+            await copyFile(join(dirPath, file), join(newDirPath, file));
+          }
+        });
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+console.log(copyFiles());
